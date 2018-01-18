9174cb15d45c4ad227e70f479a484df3c8864642
SherryHolmes <haorui5@163.com>
2018-01-18 15:06:30 +0800

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+module.exports = router.get('/', function(req, res, next) {
+  res.render('index', { title: 'SherryHolmes主页' });
+});
+
+/* GET enjoy page. */
+module.exports = router.get('/enjoy', function(req, res, next) {
+  res.render('enjoy', { title: '美句欣赏页' });
+});
+
+/* GET force page. */
+module.exports = router.get('/force', function(req, res, next) {
+  res.render('force', { title: '力导向图页' });
+});
+
+/* GET users listing. */
+module.exports = router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
